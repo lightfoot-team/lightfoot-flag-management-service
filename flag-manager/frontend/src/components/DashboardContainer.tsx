75849d5175ca71254009eaa7f083accec7a5ba8e
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-07-24 07:55:46 -0400

    Refactor dashboard and panels into dashboard container
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>

---
+++ flag-manager/frontend/src/components/DashboardContainer.tsx
@@ -0,0 +1,52 @@
+import Panel from "./Panel";
+import Dashboard from "./Dashboard"
+import { useEffect } from "react";
+import { createDashboard, getDashboard } from "../services/grafana";
+import { uids } from "../models/dashboard";
+import { redDashboardBody } from "../models/dashboard";
+
+const DashboardContainer = () => {
+  console.log('Name:', redDashboardBody.metadata.name)
+  useEffect(() => {
+    const dashboard = async () => {
+      if (uids.includes(redDashboardBody.metadata.name)) {
+        const uid = redDashboardBody.metadata.name;
+
+        const response = await getDashboard(uid);
+        return response;
+      } else {
+      const response = await createDashboard(redDashboardBody);
+      console.log(response);
+      uids.push('redDashboardBody.metadata.name')
+    }
+    }
+    dashboard()
+  }, [])
+
+  return (
+    <div className="dashboard-container">
+      {/* <iframe src="http://localhost:3002/d-solo/64daa860-5d81-4078-979a-107e984fdf57/new-dashboard?orgId=1&from=1753170283267&to=1753191883267&timezone=browser&panelId=1&__feature.dashboardSceneSolo"
+        width="450" height="200"
+      >
+      </iframe>
+      <iframe src="http://localhost:3002/d-solo/gdxccn/example-dashboard?orgId=1&from=1753170283267&to=1753191883267&timezone=browser&panelId=1&__feature.dashboardSceneSolo"
+        width="450" height="200"
+      >
+      </iframe>
+      */}
+      <Panel
+        dashboardId={redDashboardBody.metadata.name}
+        panelId={2}
+        variables={[]}
+      >
+      </Panel>
+
+      <Dashboard dashboardId={redDashboardBody.metadata.name} variables={[]}>
+      </Dashboard>
+
+    </div>
+  )
+
+}
+
+export default DashboardContainer;
