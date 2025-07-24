8e791f032f3ab772446b1c7825e27b692d02b20e
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-07-24 07:48:59 -0400

    Add routes for new observability and test form components
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>

--- flag-manager/frontend/src/App.tsx
+++ flag-manager/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import NewFlagForm from "./components/NewFlagForm"
 import Flags from "./components/Flags"
+import Dashboard from "./components/Dashboard"
+import RefactorForm from "./components/RefactorForm"
 
 function App() {
 
@@ -11,7 +13,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Flags />} />
         <Route path="flags" element={<Flags />} />
+        <Route path="flags/observability" element={<Dashboard />} />
         <Route path="flags/add" element={<NewFlagForm />} />
+        <Route path="flags/hook-form" element={<RefactorForm />} />
       </Route>
     </Routes>
     </div>
