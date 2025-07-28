a5b15a01056e9129e508af3a0f6ec90ee322eb1a
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-07-28 12:25:56 -0400

    Add Boolean and Non Boolean variant forms into edit variants form
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>

--- flag-manager/frontend/src/components/EditVariantsForm.tsx
+++ flag-manager/frontend/src/components/EditVariantsForm.tsx
@@ -1,7 +1,29 @@
+import BooleanFlagVariantInput from "./BooleanVariantInput";
+import NonBooleanVariantInput from "./NonBooleanVariantInput";
+
 const EditVariantsForm = ({ variants, onSave, onCancel, flagDetails }) => {
+  const flagType = flagDetails.flagType;
+  
   return (
     <div>
       <p>Placeholder edit variants</p>
+      {flagType === "boolean" && (
+          <BooleanFlagVariantInput 
+            register={register} 
+            errors={errors}
+            setValue={setValue}
+          />
+        )}
+        {flagType !== "boolean" && (
+          <NonBooleanVariantInput 
+            fields={fields}
+            append={append}
+            remove={remove}
+            register={register}
+            errors={errors}
+            flagType={flagType}
+          />
+        )}
       <div>
         <button onClick={onSave}>Save changes</button>
         <button onClick={onCancel}>Cancel</button>
