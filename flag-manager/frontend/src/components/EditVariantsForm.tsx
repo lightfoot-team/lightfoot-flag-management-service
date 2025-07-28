9852aff4e8ceab11846bdfbc20c317b1b9e15904
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-07-28 12:17:06 -0400

    Add Edit Variant button and placeholder component
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>

---
+++ flag-manager/frontend/src/components/EditVariantsForm.tsx
@@ -0,0 +1,13 @@
+const EditVariantsForm = ({ variants, onSave, onCancel }) => {
+  return (
+    <div>
+      <p>Placeholder edit variants</p>
+      <div>
+        <button onClick={onSave}>Save changes</button>
+        <button onClick={onCancel}>Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+export default EditVariantsForm
--- flag-manager/frontend/src/components/FlagView.tsx
+++ flag-manager/frontend/src/components/FlagView.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { type FlagDetails } from "../types/flagTypes";
 import { type UserEvaluationContext } from "../types/evaluationTypes";
 import NewRuleForm from "./NewRuleForm";
 import FlagDashboard from "./FlagDashboard";
+import EditVariantsForm from "./EditVariantsForm";
 
      
 interface FlagProps {
@@ -18,10 +20,31 @@ const testUserEvaluationContext: UserEvaluationContext = {
   location: ''
 }
 const FlagView:React.FC<FlagProps> = ({ flagDetails }) => {
+  const [ isEditingVariants, setIsEditingVariants ] = useState(false);
+
+  const handleToggleEditingVariants = () => {
+    setIsEditingVariants(!isEditingVariants);
+  }
+
+  const handleSaveVariants = (newVariants) => {
+    // Handle saving the variants
+    console.log("Saving variants:", newVariants);
+    setIsEditingVariants(false); // Hide edit form
+  };
+
+  const handleCancelEdit = () => {
+    setIsEditingVariants(false); // Hide edit form without saving
+  };
 
   return (
     <div className="space-y-8">
-  
+      {isEditingVariants ? (
+        <EditVariantsForm
+          variants={flagDetails.variants}
+          onSave={handleSaveVariants}
+          onCancel={handleCancelEdit}
+        />
+      ) : (
       <section className="bg-gray-50 rounded-lg p-6 shadow-sm border border-gray-200">
         <h2 className="text-xl font-semibold text-blue-700 mb-4">Variants:</h2>
         <div className="grid grid-cols-2 gap-4">
@@ -35,7 +58,11 @@ const FlagView:React.FC<FlagProps> = ({ flagDetails }) => {
             </div>
           ))}
         </div>
+        <div>
+          <button onClick={handleToggleEditingVariants}>Edit Variants</button>
+        </div>
       </section>
+      )}
 
       <section className="bg-white rounded-lg p-6 shadow-sm border border-gray-200">
         <NewRuleForm flag={flagDetails} contextKinds={[testUserEvaluationContext]} />
