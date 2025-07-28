f94ebdc730bcb49574ccec0db8e7406bc3a9a3a7
Allister Driscoll <allisterdrisc@gmail.com>
2025-07-28 17:16:48 -0400

    Add dashboardLoaded state to FlagDashboard and edit useEffect block
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: PatrickMayoMoran <32110056+PatrickMayoMoran@users.noreply.github.com>

--- flag-manager/frontend/src/components/FlagDashboard.tsx
+++ flag-manager/frontend/src/components/FlagDashboard.tsx
@@ -1,42 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Panel from "./Panel";
 import { byVariantDashboardBody } from "../models/dashboard";
 import { createDashboard, getDashboard } from "../services/grafana";
-import { uids } from "../models/dashboard";
 
 interface FlagDashboardProps {
   flagKey: string;
 }
 
 const FlagDashboard:React.FC<FlagDashboardProps> = ({ flagKey }) => {
-  // uids.pop();
-  useEffect(() => {
-    const dashboard = async () => {
-      try {
-        if (uids.includes(byVariantDashboardBody.metadata.name)) {
-          const uid = byVariantDashboardBody.metadata.name;
+  const [dashboardLoaded, setDashboardLoaded] = useState(false);
 
-          const response = await getDashboard(uid);
-          return response;
-        } else {
-          const response = await createDashboard(byVariantDashboardBody);
-          console.log(response);
-          uids.push(byVariantDashboardBody.metadata.name)
+  useEffect(() => {
+      const dashboard = async () => {
+        try {
+          await getDashboard(byVariantDashboardBody.metadata.name);
+          setDashboardLoaded(true);
+        } catch (error) {
+          await createDashboard(byVariantDashboardBody);
+          setDashboardLoaded(true);
+          console.error('Get Dashboard failed, creating dashboard instead.', error);
         }
-      } catch (error) {
-        console.error('Dashboard operation failed:', error);
       }
-    }
-    dashboard()
-  }, []);
+      dashboard()
+    }, [dashboardLoaded])
 
   return (
-    <div className="flex flex-col gap-4 p-4 bg-white rounded-lg shadow">
+    <>
+    {dashboardLoaded && <div className="flex flex-col gap-4 p-4 bg-white rounded-lg shadow">
       <h2 className="text-xl font-semibold mb-2">Flag Dashboard: <span className="font-mono text-blue-600">{flagKey}</span></h2>
       <Panel dashboardId={byVariantDashboardBody.metadata.name} panelId={1} variables={[flagKey]} />
       <Panel dashboardId={byVariantDashboardBody.metadata.name} panelId={2} variables={[flagKey]} />
       <Panel dashboardId={byVariantDashboardBody.metadata.name} panelId={3} variables={[flagKey]} />
-    </div>
+    </div>}
+  </>
   );
 }
 
