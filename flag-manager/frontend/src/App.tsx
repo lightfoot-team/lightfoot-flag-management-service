6bb1e1410c2e7c73d9810177533f64c5ab4b439d
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-07-29 11:04:04 -0400

    Use two different states for loading flag vs. overview dashboard
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: allisterdrisc <115250579+allisterdrisc@users.noreply.github.com>

--- flag-manager/frontend/src/App.tsx
+++ flag-manager/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import NewFlagForm from "./components/NewFlagForm"
 import Flags from "./components/Flags"
-//import DashboardContainer from "./components/ObservabilityContainer"
 import ObservabilityContainer from "./components/ObservabilityContainer"
 import { useState, useEffect } from "react"
 import FlagViewPage from "./components/FlagViewPage"
@@ -10,41 +9,45 @@ import { getDashboard, createDashboard } from "./services/grafana"
 import { redDashboardBody, byVariantDashboardBody } from "./models/dashboard"
 import './App.css'
 import Rules from "./components/Rules"
+
 function App() {
-  const [dashboardsLoaded, setDashboardsLoaded] = useState({
-    overview: false,
-    byVariant: false
-  })
+
+  const [overviewDashboardLoaded, setOverviewDashboardLoaded] = useState(false);
+  const [variantsDashboardLoaded, setVariantsDashboardLoaded] = useState(false);
+
   useEffect(() => {
     const loadOverview = async () => {
       try {
         await getDashboard(redDashboardBody.metadata.name);
-        setDashboardsLoaded({ ...dashboardsLoaded, overview: true });
+        setOverviewDashboardLoaded(true);
       } catch (error) {
         await createDashboard(redDashboardBody);
-        setDashboardsLoaded({ ...dashboardsLoaded, overview: true });
+        setOverviewDashboardLoaded(true);
       }
     }
+
     const loadByVariant = async () => {
       try {
         await getDashboard(byVariantDashboardBody.metadata.name);
-        setDashboardsLoaded({ ...dashboardsLoaded, byVariant: true });
+        setVariantsDashboardLoaded(true);
       } catch (error) {
         await createDashboard(byVariantDashboardBody);
-        setDashboardsLoaded({ ...dashboardsLoaded, byVariant: true });
+        setVariantsDashboardLoaded(true);
       }
     }
+
     loadOverview()
     loadByVariant()
-  }, [dashboardsLoaded])
+  }, [overviewDashboardLoaded, variantsDashboardLoaded])
+
   return (
     <div className="application">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Flags />} />
           <Route path="flags" element={<Flags />} />
-          <Route path="flags/:flagKey" element={<FlagViewPage flagDashboardLoaded={dashboardsLoaded.byVariant}></FlagViewPage>} />
-          <Route path="flags/observability" element={<ObservabilityContainer dashboardLoaded={dashboardsLoaded.overview}></ObservabilityContainer>} />
+          <Route path="flags/:flagKey" element={<FlagViewPage flagDashboardLoaded={variantsDashboardLoaded}></FlagViewPage>} />
+          <Route path="flags/observability" element={<ObservabilityContainer dashboardLoaded={overviewDashboardLoaded}></ObservabilityContainer>} />
           <Route path="flags/add" element={<NewFlagForm />} />
           <Route path="flags/rules" element={<Rules></Rules>} />
         </Route>
