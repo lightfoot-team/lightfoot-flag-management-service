8580c727492e5bf121b2684e479163aae1a36d1f
Scott Venkataraman <scottvenkataraman22@gmail.com>
2025-07-30 15:48:13 -0400

    Remove debugging console.logs from resolved functions/methods

--- flag-manager/frontend/src/services/flags.ts
+++ flag-manager/frontend/src/services/flags.ts
@@ -37,14 +37,12 @@ export const addFlag = async (flagFormDetails: FlagFormDetails) => {
 }
 
 export const updateFlag = async (flagFormDetails: FlagFormDetails) => {
-  console.log("Updating flag...", flagFormDetails)
   const result = await axios.post(`${baseURL}/update`, flagFormDetails, axiosConfig);
   return result;
 }
 
 export const deleteFlag = async (flagKey: string) => {
   const result = await axios.delete(`${baseURL}/${flagKey}`);
-  console.log('result:', result);
   return result;
 }
 
