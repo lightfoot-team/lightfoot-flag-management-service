cb5ad2f4f4151eca12704e02ba70dbbfb1698a0e
Scott Venkataraman <scottvenkataraman22@gmail.com>
2025-08-02 15:17:13 -0400

    Remove evaluation api from flag manager

--- flag-manager/evaluation-api/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import { getFlagEvaluation, getFlagEvaluationConfig } from '../controllers/controller';
-
-const router = Router();
-
-
-router.post('/', getFlagEvaluation);
-router.post('/config', getFlagEvaluationConfig)
-export default router;
