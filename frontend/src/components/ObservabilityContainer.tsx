8f57fec018f9b43dc89218dba07e6f64406fb831
Allister Driscoll <allisterdrisc@gmail.com>
2025-08-11 16:11:25 -0400

    Add link to grafana's UI in observability tab
    Co-authored-by: alexwjohnson964 <162201012+alexwjohnson964@users.noreply.github.com>
    Co-authored-by: ScottVenkataraman <109530825+ScottVenkataraman@users.noreply.github.com>
    Co-authored-by: PatrickMayoMoran <32110056+PatrickMayoMoran@users.noreply.github.com>

--- frontend/src/components/ObservabilityContainer.tsx
+++ frontend/src/components/ObservabilityContainer.tsx
@@ -6,8 +6,16 @@ interface ObservabilityContainerProps {
 }
 
 const ObservabilityContainer:React.FC<ObservabilityContainerProps> = ({ dashboardLoaded }) => {
+  const handleClick = () => {
+    const grafanaUrl = import.meta.env.VITE_GRAFANA_URL || 'http://localhost:3002';
+    window.open(grafanaUrl, '_blank');
+  }
+
   return (
     <>
+      <div className="flex items-center justify-between mb-6 pr-4">
+        <button className="px-4 py-2 rounded-md text-base bg-blue-200 text-blue-1000 hover:bg-blue-300 transition" onClick={handleClick}>To Grafana UI</button>
+      </div>
       {dashboardLoaded && (
         <div className="p-4 bg-white rounded-lg shadow">
           <div className="grid grid-cols-2 gap-6">
